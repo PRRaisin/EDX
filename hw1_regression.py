fc10421d5a80fce924d6eda424e5db240f74f70e
PRRaisin <philippe.raisin@icloud.com>
2017-04-01 14:44:51 +0200

    finished part1 of the project

--- hw1_regression.py
+++ hw1_regression.py
@@ -11,70 +11,66 @@ import matplotlib.pyplot as plt
 
 # This code implements the K-means clustering.
 from numpy import genfromtxt
-#lamb, sigma2, X_train,  y_train, X_test  = genfromtxt(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5],delimiter=',')
+
+
+# read out command line arguments
+lamb = float(sys.argv[1]) 
+sigma2 = float(sys.argv[2])
+X_train = genfromtxt(sys.argv[3], delimiter=',') 
+y_train = genfromtxt(sys.argv[4], delimiter=',')
+X_test = genfromtxt(sys.argv[5], delimiter=',')
+
+#X_train = X_train[:, np.newaxis]
+
+
+#lamba sigma2 X_train.csv y_train.csv X_test.csv =  sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5],delimiter=',')
 
 #sigma2 = float(sigma2)
 #lamb   = float(lamb)
-
+'''
 lamb = 1
-
 X_train = np.array([258, 270, 294, 320, 342, 368, 396, 446, 480, 586])
-X_train = X_train[:, np.newaxis]
-y_train = np.array([236.4, 234.4, 252.8, 298.6, 314.2, 342.2, 360.8, 368, 391.2, 390.8])
-y_train = y_train[:, np.newaxis]
-
-np.savetxt('X_train.csv', X_train, delimiter=',')
-np.savetxt('y_train.csv', X_train, delimiter=',')
-np.savetext('X_test.csv', X_test, delimiter = ',')
-
-print(X_train)
-
-
-
+X_train = np.vstack((X_train, np.ones(X_train.shape[0])))
+X_train = np.transpose(X_train)
+'''
 # get the dimensions
 [n,d] = X_train.shape # n samples, d dimensions 
+
+'''
+X_test = X_train
+X_test[:,0] = X_train[:,0]*np.random.rand(n)*2
+y_train = np.array([236.4, 234.4, 252.8, 298.6, 314.2, 342.2, 360.8, 368, 391.2, 390.8])
+y_train = y_train[:, np.newaxis]
+np.savetxt('X_train.csv', X_train, delimiter=',')
+np.savetxt('y_train.csv', y_train, delimiter=',')
+np.savetxt('X_test.csv',  X_test, delimiter = ',')
+'''
 
 # preprocess the data 
 
 # subtract the mean
 y_train_mean = np.mean(y_train)
-y_train = y_train-y_train_mean
+
+#y_train = y_train-y_train_mean
 
 # standardize the dimensions
 X_means = np.mean(X_train, axis=0)
 X_std = np.std(X_train, axis=0)
-X_train = (X_train-X_means)/X_std
+#X_train = (X_train-X_means)/X_std
 
 # calculate the linear ridge regression weights wRR
 A = lamb*np.eye(d)+np.dot(np.transpose(X_train),X_train)
 b = np.dot(np.transpose(X_train),y_train)
 wRR = solve(A,b)
 print(wRR)
-np.savetxt(['wRR','.csv'], wRR, delimiter=',')
+# print the wRR to the file
+np.savetxt(''.join(['wRR_',str(int(lamb)),'.csv']), wRR, delimiter=',')
 
 
-plt.plot(X_train, y_train+y_train_mean, marker='s')
-plt.plot(X_train, np.dot(X_train,wRR)+y_train_mean)
+plt.plot(X_train[:,0], y_train, marker='s', linestyle='None')
+plt.plot(X_train[:,0], np.dot(X_train,wRR), marker ='<')
 plt.show()
 
-
-# print the wRR to the file
-
-# apply wRR on the test data set. Remember to first subtract the old mean from the new data
-#X_test_normalized = (X_test - X_means) / sigma2
-
-#y_test = wRR * X_test_normalized
-
-
-
-
-
-
-# write the results to the files
-#np.savetxt(''.join(['centroids-',str(i+1),'.csv']), cent, delimiter=',')
-
-
-
-#plt.scatter(X[:,0],X[:,1])
-#plt.scatter(cent[:,0], cent[:,1], marker='s', color='g')
+#plt.plot(X_train, y_train+y_train_mean, marker='s')
+#plt.plot(X_train, np.dot(X_train,wRR)+y_train_mean)
 #plt.show()
