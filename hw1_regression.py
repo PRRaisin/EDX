59ea3058a95647da74f996085fab06e271c1ea21
PRRaisin <philippe.raisin@icloud.com>
2017-04-02 12:46:52 +0200

    finished first version of ridge regression part 2

--- hw1_regression.py
+++ hw1_regression.py
@@ -5,7 +5,7 @@ import numpy as np
 import sys
 import csv
 from numpy.random import choice, randint
-from numpy.linalg import solve
+from numpy.linalg import solve, inv
 from scipy.spatial.distance import cdist
 import matplotlib.pyplot as plt
 
@@ -19,6 +19,7 @@ sigma2 = float(sys.argv[2])
 X_train = genfromtxt(sys.argv[3], delimiter=',') 
 y_train = genfromtxt(sys.argv[4], delimiter=',')
 X_test = genfromtxt(sys.argv[5], delimiter=',')
+
 
 #X_train = X_train[:, np.newaxis]
 
@@ -62,14 +63,72 @@ X_std = np.std(X_train, axis=0)
 A = lamb*np.eye(d)+np.dot(np.transpose(X_train),X_train)
 b = np.dot(np.transpose(X_train),y_train)
 wRR = solve(A,b)
-print(wRR)
 # print the wRR to the file
 np.savetxt(''.join(['wRR_',str(int(lamb)),'.csv']), wRR, delimiter=',')
 
 
+
+
+
+# part II: Active Learning
+
+# calculate Sigma (the mu is the wRR)
+
+# implement the active learning on page 61 in the script: 
+maxIt = 10
+
+n2 = X_test.shape[0]
+
+sigma02 = np.zeros((n2,1))  
+Xnew = X_test
+Xmodel = X_train
+ymodel = y_train
+chosenIndices = np.zeros((1,10))
+
+
+
+for i in range(maxIt):
+
+	n2 = Xmodel.shape[0];
+	# calculate the updated sigma matrix 
+	Sigma = inv(lamb*np.eye(d)+1/sigma2*np.dot(np.transpose(Xmodel),Xmodel))
+
+	# calculate the updated wRR (the mu)
+	A = lamb * sigma2 * np.eye(d) + np.dot(np.transpose(Xmodel),Xmodel)
+	b = np.dot(np.transpose(Xmodel),ymodel)
+	wRR = solve(A,b)
+	wRR = wRR[:,np.newaxis]
+	# Incorporate a new datapoint
+	# calculate the covariances for each new datapoint
+	sigma02 = sigma2 + np.dot(np.dot(Xnew, Sigma),np.transpose(Xnew))
+	sigma02 = np.diag(sigma02)
+
+
+	# find the x0 with the highest sigma
+	maxIndex = sigma02.argmax()
+	Xmodel = np.vstack((Xmodel, Xnew[maxIndex,:]))
+	ynew = np.dot(Xnew[maxIndex,:], wRR)
+	ymodel = np.hstack((ymodel, ynew))
+
+	originIndex = np.where((X_test == Xnew[maxIndex,:]).all(axis=1))
+	chosenIndices[0,i] = originIndex[0][0]+1
+	# delete the picked out datapoint from the array of new obervations
+	Xnew = np.delete(Xnew, maxIndex, 0)
+
+# write the indices to the file 
+np.savetxt(''.join(['active_',str(int(lamb)),'_',str(int(sigma2)),'.csv']), chosenIndices, delimiter=',')
+
+'''
 plt.plot(X_train[:,0], y_train, marker='s', linestyle='None')
-plt.plot(X_train[:,0], np.dot(X_train,wRR), marker ='<')
+plt.plot(Xmodel[:,0], np.dot(Xmodel,wRR), marker ='<')
 plt.show()
+'''
+
+
+
+
+
+
 
 #plt.plot(X_train, y_train+y_train_mean, marker='s')
 #plt.plot(X_train, np.dot(X_train,wRR)+y_train_mean)
