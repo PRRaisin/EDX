c05a0c8553a0b4691d90d344650c7c7dddbc8dcb
PRRaisin <philippe.raisin@icloud.com>
2017-04-03 17:26:46 +0200

    finished project 3, but needs to get rid of for loops"
    0

--- hw3_clustering.py
+++ hw3_clustering.py
@@ -1,13 +1,58 @@
 
-#from sklearn.datasets import make_blobs
 from os.path import join
 import numpy as np 
 import sys
 import csv
 from numpy.random import choice, randint
 from scipy.spatial.distance import cdist
-import matplotlib.pyplot as plt
 from scipy.stats import multivariate_normal
+import copy
+#from matplotlib.patches import Ellipse
+#from sklearn.datasets import make_blobs
+#import matplotlib.pyplot as plt
+#from matplotlib.mlab import bivariate_normal
+
+
+
+def plot_cov_ellipse(cov, pos, nstd=1, ax=None, **kwargs):
+    """
+    Plots an `nstd` sigma error ellipse based on the specified covariance
+    matrix (`cov`). Additional keyword arguments are passed on to the 
+    ellipse patch artist.
+
+    Parameters
+    ----------
+        cov : The 2x2 covariance matrix to base the ellipse on
+        pos : The location of the center of the ellipse. Expects a 2-element
+            sequence of [x0, y0].
+        nstd : The radius of the ellipse in numbers of standard deviations.
+            Defaults to 2 standard deviations.
+        ax : The axis that the ellipse will be plotted on. Defaults to the 
+            current axis.
+        Additional keyword arguments are pass on to the ellipse patch.
+
+    Returns
+    -------
+        A matplotlib ellipse artist
+    """
+    def eigsorted(cov):
+        vals, vecs = np.linalg.eigh(cov)
+        order = vals.argsort()[::-1]
+        return vals[order], vecs[:,order]
+
+    if ax is None:
+        ax = plt.gca()
+
+    vals, vecs = eigsorted(cov)
+    theta = np.degrees(np.arctan2(*vecs[:,0][::-1]))
+
+    # Width and height are "full" widths, not radius
+    width, height = 2 * nstd * np.sqrt(vals)
+    ellip = Ellipse(xy=pos, width=width, height=height, angle=theta, **kwargs)
+
+    ax.add_artist(ellip)
+    return ellip
+
 
 
 # This code implements the K-means clustering.
@@ -24,8 +69,10 @@ maxIt = 10
 
 ### Part I: k-means clustering ###
 
-# chose the first k centroids randomly 
-cent = X[randint(n, size=k), :]
+# chose the first k centroids randomly (also assign the same )
+initialCent = X[randint(n, size=k), :]
+cent = initialCent 
+
 
 for i in range(maxIt):
 
@@ -43,30 +90,20 @@ for i in range(maxIt):
 	np.savetxt(''.join(['centroids-',str(i+1),'.csv']), cent, delimiter=',')
 
 
-'''
-plt.scatter(X[:,0],X[:,1])
-plt.scatter(cent[:,0], cent[:,1], marker='s', color='g')
-plt.show()
-'''
-
-
-
-
 ### Part II: The EM Algorithm ###
 # Implementation is according to the description on page 194 of the script, on Gaussian Mixture models
-
-
 prior = (1./k)*np.ones((k,1))
 Sigma = np.zeros((k,d,d))
+
 for i in range(k):
 	Sigma[i,:,:] = np.eye(d)
 
-mu = X[randint(n, size=k), :]
+
+mu = copy.deepcopy(initialCent)
 
 #print(mu.shape)
 
-for i in range(maxIt):
-
+for it in range(maxIt):
 
 	# the E-step
 	phi = np.zeros((n,k))
@@ -78,41 +115,59 @@ for i in range(maxIt):
 		# iterate over each cluster
 		for m in range(k):
 			pdf = multivariate_normal(mean = mu[m,:], cov = Sigma[m,:,:])
-			multVar = pdf.pdf(X[j,:])
-			phi[j,m] = prior[m]*multVar
-			multVarSum =multVarSum + multVar
-
-		phi[j,:] = phi[j,:]/multVarSum
+			phi[j,m] = prior[m]*pdf.pdf(X[j,:])
+			
+		#print(phi[j,:])
+		#print(np.sum(phi[j,:]))
+		#print('---------')
+		phi[j,:] = phi[j,:]/np.sum(phi[j,:],0)
 
 	# the M-step
-	print(phi)
 	n_k = np.sum(phi,0)
-	print(n_k)
 	# update the prior
 	prior = n_k/n	
+
+	# initilize the mu and sigma vector
+	mu = np.zeros((k,d))
+	Sigma = np.zeros((k,d,d))
 	
 	for m in range(k):
 
 		for i in range(n):
 			mu[m,:] =  mu[m,:] + phi[i,m]*X[i,:] 
-
-		#print(n_k[m])
+			
 		mu[m,:] = mu[m,:]/n_k[m]
 
+
+		#print(mu[m,:])
 		for i in range(n):
-			Sigma[m,:,:] = Sigma[m,:,:] + phi[i,m] * np.dot(np.transpose(X[i,:] - mu[m,:]), (X[i,:] - mu[m,:]))
+
+			#print(diff.shape)
+			Sigma[m,:,:] = Sigma[m,:,:] + phi[i,m] * np.outer(X[i,:] - mu[m,:], X[i,:] - mu[m,:] )
 
 
+		#print(Sigma[m,:,:])
+		Sigma[m,:,:] = Sigma[m,:,:]/n_k[m]
 
 
+		# save the covariance matrix
+		np.savetxt(''.join(['Sigma-',str(m+1),'-',str(it+1),'.csv']), Sigma[m,:,:], delimiter=',')
 
+	#print(mu)
+	# save the stuff
+	np.savetxt(''.join(['pi-',str(it+1),'.csv']), prior, delimiter=',')
+	np.savetxt(''.join(['mu-',str(it+1),'.csv']), mu, delimiter=',')
 
-
-
-
-
-
-
-
+'''
+## plot some results ##
+plt.figure()
+ax = plt.gca()
+plt.scatter(X[:,0],X[:,1])
+plt.scatter(cent[:,0], cent[:,1], marker='d', color='g')
+plt.scatter(mu[:,0], mu[:,1], marker='s', color = 'y')
+#for i in range(k):
+#	plot_cov_ellipse(Sigma[i,:,:], mu[i,:], nstd=1, ax=ax)
+plt.show()
+'''
 
 
